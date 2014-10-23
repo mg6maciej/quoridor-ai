a5ebd3500f20de5db7a62e3cdc286362d01c7443
Maciej Górski <maciek.gorski@gmail.com>
2014-10-24 01:59:12 +0200

    added Position interface

---
+++ model/position.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"gopkg.in/fatih/set.v0"
+)
+
+type Position interface {
+	Finished() bool
+	Move(move string) Position
+	Takeback() Position
+	White() string
+	Black() string
+	WhiteActive() bool
+	WhiteWallsLeft() int
+	BlackWallsLeft() int
+	Eval() int
+	LegalMoves() *set.Set
+}

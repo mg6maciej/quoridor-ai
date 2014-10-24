e674b7fa124516538133bac59ef313c8e89e934d
Maciej Górski <maciek.gorski@gmail.com>
2014-10-24 02:02:17 +0200

    added cpu profiling code

--- main/main.go
+++ main/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/mg6maciej/quoridor-ai/ai"
 	"github.com/mg6maciej/quoridor-ai/model"
+	"log"
+	"os"
+	"runtime/pprof"
 	"time"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 	pos := model.NewPosition()
-	for i := 1; i <= 10; i++ {
+	for i := 1; i <= 3; i++ {
 		start := time.Now()
 		moves := ai.AlphaBeta(pos, i)
 		elapsed := time.Since(start)

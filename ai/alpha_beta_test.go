152592964911523e99f8c0075ff26a7ffea0676a
Maciej Górski <maciek.gorski@gmail.com>
2014-10-25 18:07:31 +0200

    deeper AlphaBeta test

--- ai/alpha_beta_test.go
+++ ai/alpha_beta_test.go
@@ -6,10 +6,27 @@ import (
 )
 
 func TestChoosesBetterMove(t *testing.T) {
-	root := &MockPosition{evalFunc: func() int { panic("unsupported") }, whiteActive: true}
+	root := makeRoot()
 	child1 := &MockPosition{eval: 0}
-	child2 := &MockPosition{eval: 1}
+	child2 := &MockPosition{eval: 3}
 	root.children = map[string]*MockPosition{"c1": child1, "c2": child2}
 	move := AlphaBeta(root, 1)
 	assert.Equal(t, "c2", move)
+}
+
+func TestChoosesBetterDeep(t *testing.T) {
+	root := makeRoot()
+	child1 := &MockPosition{eval: 0}
+	child11 := &MockPosition{eval: 2}
+	child1.children = map[string]*MockPosition{"c11": child11}
+	child2 := &MockPosition{eval: 3}
+	child21 := &MockPosition{eval: -1}
+	child2.children = map[string]*MockPosition{"c21": child21}
+	root.children = map[string]*MockPosition{"c1": child1, "c2": child2}
+	move := AlphaBeta(root, 2)
+	assert.Equal(t, "c1", move)
+}
+
+func makeRoot() *MockPosition {
+	return &MockPosition{evalFunc: func() int { panic("unsupported") }, whiteActive: true}
 }

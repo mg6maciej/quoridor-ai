a06c726810cf2f83974d37c69aca56b0cff8d847
Maciej Górski <maciek.gorski@gmail.com>
2014-10-25 15:46:48 +0200

    LegalMoves and Finished as params to AlphaBeta

--- ai/alpha_beta.go
+++ ai/alpha_beta.go
@@ -7,20 +7,24 @@ import (
 
 type EvalFunc func(model.Position) int
 
-func AlphaBeta(pos model.Position, depth int, eval EvalFunc) string {
-	_, bestMove := alphaBetaImpl(pos, depth, eval, -1000000000, 1000000000)
+type LegalMovesFunc func(model.Position) *set.Set
+
+type FinishedFunc func(model.Position) bool
+
+func AlphaBeta(pos model.Position, depth int, eval EvalFunc, legalMoves LegalMovesFunc, finished FinishedFunc) string {
+	_, bestMove := alphaBetaImpl(pos, depth, eval, legalMoves, finished, -1000000000, 1000000000)
 	return bestMove
 }
 
-func alphaBetaImpl(pos model.Position, depth int, eval EvalFunc, alpha int, beta int) (int, string) {
-	if depth == 0 || model.Finished(pos) {
+func alphaBetaImpl(pos model.Position, depth int, eval EvalFunc, legalMoves LegalMovesFunc, finished FinishedFunc, alpha int, beta int) (int, string) {
+	if depth == 0 || finished(pos) {
 		return eval(pos), ""
 	}
 	if pos.WhiteActive() {
 		var bestMove string
-		for _, move := range set.StringSlice(model.LegalMoves(pos)) {
+		for _, move := range set.StringSlice(legalMoves(pos)) {
 			pos = pos.Move(move)
-			value, _ := alphaBetaImpl(pos, depth-1, eval, alpha, beta)
+			value, _ := alphaBetaImpl(pos, depth-1, eval, legalMoves, finished, alpha, beta)
 			pos = pos.Takeback()
 			if alpha < value {
 				alpha = value
@@ -33,9 +37,9 @@ func alphaBetaImpl(pos model.Position, depth int, eval EvalFunc, alpha int, beta
 		return alpha, bestMove
 	} else {
 		var bestMove string
-		for _, move := range set.StringSlice(model.LegalMoves(pos)) {
+		for _, move := range set.StringSlice(legalMoves(pos)) {
 			pos = pos.Move(move)
-			value, _ := alphaBetaImpl(pos, depth-1, eval, alpha, beta)
+			value, _ := alphaBetaImpl(pos, depth-1, eval, legalMoves, finished, alpha, beta)
 			pos = pos.Takeback()
 			if beta > value {
 				beta = value
--- ai/alpha_beta_test.go
+++ ai/alpha_beta_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestMoveForwardAsFirstBestMove(t *testing.T) {
 	pos := model.NewPosition()
-	move := AlphaBeta(pos, 1, model.Eval)
+	move := AlphaBeta(pos, 1, model.Eval, model.LegalMoves, model.Finished)
 	assert.Equal(t, "e2", move)
 }
 
 func TestMoveForwardAsSecondBestMove(t *testing.T) {
 	pos := model.NewPosition("e2")
-	move := AlphaBeta(pos, 1, model.Eval)
+	move := AlphaBeta(pos, 1, model.Eval, model.LegalMoves, model.Finished)
 	assert.Equal(t, "e8", move)
 }

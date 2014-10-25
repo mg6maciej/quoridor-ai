8b00de006309e1e56f87f73fb4f1aac953f43be9
Maciej Górski <maciek.gorski@gmail.com>
2014-10-25 18:24:55 +0200

    refactoring: DSL to create tree of mock positions

--- ai/alpha_beta_test.go
+++ ai/alpha_beta_test.go
@@ -6,27 +6,34 @@ import (
 )
 
 func TestChoosesBetterMove(t *testing.T) {
-	root := makeRoot()
-	child1 := &MockPosition{eval: 0}
-	child2 := &MockPosition{eval: 3}
-	root.children = map[string]*MockPosition{"c1": child1, "c2": child2}
+	root := makeRoot().setChildren(
+		"c1", &MockPosition{eval: 0},
+		"c2", &MockPosition{eval: 3},
+	)
 	move := AlphaBeta(root, 1)
 	assert.Equal(t, "c2", move)
 }
 
 func TestChoosesBetterDeep(t *testing.T) {
-	root := makeRoot()
-	child1 := &MockPosition{eval: 0}
-	child11 := &MockPosition{eval: 2}
-	child1.children = map[string]*MockPosition{"c11": child11}
-	child2 := &MockPosition{eval: 3}
-	child21 := &MockPosition{eval: -1}
-	child2.children = map[string]*MockPosition{"c21": child21}
-	root.children = map[string]*MockPosition{"c1": child1, "c2": child2}
+	root := makeRoot().setChildren(
+		"c1", (&MockPosition{eval: 0}).setChildren(
+			"c11", (&MockPosition{eval: 2}),
+		), "c2", (&MockPosition{eval: 3}).setChildren(
+			"c21", (&MockPosition{eval: -1}),
+		),
+	)
 	move := AlphaBeta(root, 2)
 	assert.Equal(t, "c1", move)
 }
 
 func makeRoot() *MockPosition {
 	return &MockPosition{evalFunc: func() int { panic("unsupported") }, whiteActive: true}
+}
+
+func (parent *MockPosition) setChildren(children ...interface{}) *MockPosition {
+	parent.children = make(map[string]*MockPosition)
+	for i := 0; i < len(children); i += 2 {
+		parent.children[children[i].(string)] = children[i+1].(*MockPosition)
+	}
+	return parent
 }
